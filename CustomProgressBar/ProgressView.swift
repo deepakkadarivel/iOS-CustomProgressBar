225c18bb253bcacf0ec5aadaf0a8b59ad0ce279a
Deepak <deepak.kadarivel@gmail.com>
2016-01-29 15:20:20 +0530

    Simple progress View

--- CustomProgressBar/ProgressView.swift
+++ CustomProgressBar/ProgressView.swift
@@ -11,16 +11,54 @@ import UIKit
 class ProgressView: UIView {
 
     private let progressLayer: CAShapeLayer = CAShapeLayer()
-    private let progressLabel: UILabel
+    
+    private var progressLabel: UILabel
     
     required init?(coder aDecoder: NSCoder) {
         progressLabel = UILabel()
         super.init(coder: aDecoder)
+        createProgressLayer()
+        createLabel()
     }
     
     override init(frame: CGRect) {
         progressLabel = UILabel()
         super.init(frame: frame)
+        createProgressLayer()
+        createLabel()
+    }
+    
+    func createLabel() {
+        progressLabel = UILabel(frame: CGRectMake(0.0, 0.0, CGRectGetWidth(frame), 60.0))
+        progressLabel.textColor = UIColor.whiteColor()
+        progressLabel.textAlignment = .Center
+        progressLabel.text = "Load Content"
+        progressLabel.font = UIFont(name: "HelveticaNeue-UltraLight", size: 40.0)
+        progressLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(progressLabel)
+        
+        addConstraint(NSLayoutConstraint(item: self, attribute: .CenterX, relatedBy: .Equal, toItem: progressLabel, attribute: .CenterX, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: self, attribute: .CenterY, relatedBy: .Equal, toItem: progressLabel, attribute: .CenterY, multiplier: 1.0, constant: 0.0))
+    }
+    
+    private func createProgressLayer() {
+        let startAngle = CGFloat(M_PI_2)
+        let endAngle = CGFloat(M_PI * 2 + M_PI_2)
+        let centerPoint = CGPointMake(CGRectGetWidth(frame)/2, CGRectGetHeight(frame)/2)
+        
+        print("startAngle: \(startAngle)  endAngle: \(endAngle)  centerPoint: \(centerPoint)")
+        
+        let gradientMaskLayer = gradientMask()
+        progressLayer.path = UIBezierPath(arcCenter: centerPoint, radius: CGRectGetWidth(frame)/2 - 30.0, startAngle: startAngle, endAngle: endAngle, clockwise: true).CGPath
+        progressLayer.backgroundColor = UIColor.clearColor().CGColor
+        progressLayer.fillColor = nil
+        progressLayer.strokeColor = UIColor.blackColor().CGColor
+        progressLayer.lineWidth = 4.0
+        progressLayer.strokeStart = 0.0
+        progressLayer.strokeEnd = 0.0
+        
+        gradientMaskLayer.mask = progressLayer
+        layer.addSublayer(gradientMaskLayer)
     }
 
     private func gradientMask() -> CAGradientLayer {
@@ -35,5 +73,30 @@ class ProgressView: UIView {
         gradientLayer.colors = arrayOfColors
         
         return gradientLayer
+    }
+    
+    func animateProgressView() {
+        progressLabel.text = "Loading...."
+        progressLayer.strokeEnd = 0.0
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = CGFloat(0.0)
+        animation.toValue = CGFloat(1.0)
+        animation.duration = 1.0
+        animation.delegate = self
+        animation.removedOnCompletion = false
+        animation.additive = true
+        animation.fillMode = kCAFillModeForwards
+        progressLayer.addAnimation(animation, forKey: "strokeEnd")
+    }
+    
+    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        progressLabel.text = "Done"
+    }
+    
+    func hideProgressView() {
+        progressLayer.strokeEnd = 0.0
+        progressLayer.removeAllAnimations()
+        progressLabel.text = "Load Content"
     }
 }
--- CustomProgressBar/ViewController.swift
+++ CustomProgressBar/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressView: ProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = UIColor(red: 52.0/255.0, green: 170.0/255.0, blue: 220.0/255.0, alpha: 1.0)
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
     }
 
+    @IBAction func hideProgress(sender: AnyObject) {
+        progressView.hideProgressView()
+    }
 
+    @IBAction func animateProgress(sender: AnyObject) {
+        progressView.animateProgressView()
+    }
 }
 
